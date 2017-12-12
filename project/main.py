48166bb8ef86c0adbeb9ce4a36bf13fd9f52364c
matan <dev.matan@gmail.com>
2017-12-12 20:33:13 +0200

    initial commit

---
+++ project/main.py
@@ -0,0 +1,10 @@
+import sys
+import dynet as dy
+import numpy as np
+
+def main():
+    """Main entry point for the script."""
+    print("hello")
+
+if __name__ == '__main__':
+    sys.exit(main())
